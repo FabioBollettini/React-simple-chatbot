83bc939c59e7c2eb937cbff6743118a7292e359b
FabioBollettini <bollettinifabio@yahoo.com>
2025-09-02 19:17:06 +0200

    Init + Sidebar component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Main.css'
+
+function Main() {
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Chat</p>
+        </div>
+    </div>
+  )
+}
+
+export default Main
